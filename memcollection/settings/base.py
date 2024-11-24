dd3230f7b9774c8b6f3e175292d98b4d77258c11
Megan McCarty <megan_mccarty@hotmail.com>
2024-11-23 20:15:24 -0500

    Bump version in base.py

--- memcollection/settings/base.py
+++ memcollection/settings/base.py
@@ -20,6 +20,9 @@ load_dotenv()
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
 
+# Project version
+VERSION = "0.4.0"
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
